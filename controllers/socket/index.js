592abec63643fb56e7821fbaefa1afcc040318ae
Muowee <33558026+Muowee@users.noreply.github.com>
2017-12-07 09:19:25 +0000

    Add jobcard socket controller

--- controllers/socket/index.js
+++ controllers/socket/index.js
@@ -4,8 +4,10 @@ module.exports = function(io){
     
     var dash = io.of('/dashboard');
     var dept = io.of('/department');
+    var jbcard = io.of('/job-card');
 
     var dashboard = require('./dashboard')(dash);
     var department = require('./department')(dept);
+    var jobCard = require('./jobCard')(jbcard);
 
 }
---
+++ controllers/socket/jobCard.js
@@ -0,0 +1,13 @@
+module.exports = function(jbCard){
+
+    jbCard.on('connection', (socket)=>{
+        console.log("A client is connected");
+        socket.emit('message', 'You are connected to dashboard!!');
+            
+        socket.broadcast.emit('message', 'Another client has just connected!');
+        socket.on('message', (data) => {
+            console.log(data); 
+            socket.broadcast.emit('message',data);
+        });
+    });
+}
