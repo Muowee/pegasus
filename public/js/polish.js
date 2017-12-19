d718c91d541edef60544a9cfa453f186c4160587
xbenyx <benitodm90@gmail.com>
2017-12-19 16:22:30 +0000

    css, html and javascript
    bottoms in the different tables

--- public/js/polish.js
+++ public/js/polish.js
@@ -7,139 +7,148 @@ socket.on('message',(data)=>{
     console.log(data);
 });
 
-$(document).ready(()=>{
-
-  $(".mybtn").on("click",function(){
-    $(this).attr("data-value");
+var Tablepolish = new Vue({
+    el: '#Tablepolish',
+    data: {
+      currentPage: 1,
+      elementsPerPage: 20000000,
+      ascending: false,
+      sortColumn: '',
+      rows: [
+        { id: 1,EstimateTime: "00:25:00", PulledDate:"21/12/2017", Qty: "5", Finished: 'Antique Silver' ,Bin:'2737',DueDate: '21/12/2017', priority:'' },
+        { id: 2,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Brass',Bin:'2737',DueDate: '21/12/2017'  , priority:'' },
+        { id: 3,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' , priority:'' },
+        { id: 4,EstimateTime: "",PulledDate:"21/12/2017", Qty: "9", Finished: 'Antique Brass',Bin:'2737',DueDate: '21/12/2017'  , priority:'' },
+        { id: 5,EstimateTime: "",PulledDate:"21/12/2017", Qty: "3", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' , priority:'' },
+        { id: 6,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' , priority:'' },
+        { id: 32,EstimateTime: "",PulledDate:"21/12/2017", Qty: "3", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017', priority:'' },
+        { id: 7,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' , priority:'' },
+        { id: 8,EstimateTime: "",PulledDate:"21/12/2017", Qty: "3", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' , priority:'' },
+        { id: 9,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' , priority:'' },
+        { id: 10,EstimateTime: "",PulledDate:"21/12/2017", Qty: "3", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017', priority:'' },
+        { id: 11,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017', priority:'' },
+        { id: 12,EstimateTime: "",PulledDate:"21/12/2017", Qty: "3", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017', priority:'' },
+        { id: 13,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017', priority:'' },
+        { id: 14,EstimateTime: "",PulledDate:"21/12/2017", Qty: "3", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017', priority:'' },
+        { id: 15,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017', priority:'' },
+        { id: 16,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017', priority:'' },
+        { id: 17,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017', priority:'' },
+        { id: 18,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017', priority:'' },
+        { id: 19,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017', priority:'' },
+        { id: 20,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017', priority:'' },
+        { id: 21,EstimateTime: "",PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017', priority:'' },
+      ]
+    },
+    methods: {
+      "sortTable": function sortTable(col) {
+        if (this.sortColumn === col) {
+          this.ascending = !this.ascending;
+        } else {
+          this.ascending = true;
+          this.sortColumn = col;
+        }
+  
+        var ascending = this.ascending;
+  
+        this.rows.sort(function(a, b) {
+          if (a[col] > b[col]) {
+            return ascending ? 1 : -1
+          } else if (a[col] < b[col]) {
+            return ascending ? -1 : 1
+          }
+          return 0;
+        })
+      },
+      "num_pages": function num_pages() {
+        return Math.ceil(this.rows.length / this.elementsPerPage);
+      },
+      "get_rows": function get_rows() {
+        var start = (this.currentPage-1) * this.elementsPerPage;
+        var end = start + this.elementsPerPage;
+        return this.rows.slice(start, end);
+      },
+      "change_page": function change_page(page) {
+        this.currentPage = page;
+      }
+    },
+    computed: {
+      "columns": function columns() {
+        if (this.rows.length == 0) {
+          return [];
+        }
+        return Object.keys(this.rows[0])
+      }
+    }
   });
 
-    var Tablepolish = new Vue({
-        el: '#Tablepolish',
-        data: {
-          currentPage: 1,
-          elementsPerPage: 9,
-          ascending: false,
-          sortColumn: '',
-          rows: [
-            { id: 1,PulledDate:"21/12/2017", Qty: "5", Finished: 'Antique Silver' ,Bin:'2737',DueDate: '21/12/2017'},
-            { id: 2,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Brass',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 3,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017'},
-            { id: 4,PulledDate:"21/12/2017", Qty: "9", Finished: 'Antique Brass',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 5,PulledDate:"21/12/2017", Qty: "3", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 6,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 32,PulledDate:"21/12/2017", Qty: "3", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 7,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 8,PulledDate:"21/12/2017", Qty: "3", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 9,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 10,PulledDate:"21/12/2017", Qty: "3", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 11,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 12,PulledDate:"21/12/2017", Qty: "3", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 13,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 14,PulledDate:"21/12/2017", Qty: "3", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 15,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 16,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 17,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 18,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 19,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 20,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-            { id: 21,PulledDate:"21/12/2017", Qty: "8", Finished: 'Antique Silver',Bin:'2737',DueDate: '21/12/2017' },
-          ]
-        },
-        methods: {
-          "sortTable": function sortTable(col) {
-            if (this.sortColumn === col) {
-              this.ascending = !this.ascending;
-            } else {
-              this.ascending = true;
-              this.sortColumn = col;
-            }
-      
-            var ascending = this.ascending;
-      
-            this.rows.sort(function(a, b) {
-              if (a[col] > b[col]) {
-                return ascending ? 1 : -1
-              } else if (a[col] < b[col]) {
-                return ascending ? -1 : 1
-              }
-              return 0;
-            })
-          },
-          "num_pages": function num_pages() {
-            return Math.ceil(this.rows.length / this.elementsPerPage);
-          },
-          "get_rows": function get_rows() {
-            var start = (this.currentPage-1) * this.elementsPerPage;
-            var end = start + this.elementsPerPage;
-            return this.rows.slice(start, end);
-          },
-          "change_page": function change_page(page) {
-            this.currentPage = page;
-          }
-        },
-        computed: {
-          "columns": function columns() {
-            if (this.rows.length == 0) {
-              return [];
-            }
-            return Object.keys(this.rows[0])
-          }
-        }
-      });
+$(document).ready(()=>{
+    //render all select
+    $('.modal').modal({});
+    $(".mybtn").on("click",function(){
+        $(this).attr("data-value");
+    });
+    
+    
 
-      Materialize.toast('Hi there!', 4000)
+    
+      // Alert
+    
+      Materialize.toast('Welcome to the Polish Department!', 4000)
 
-      // BIG Bottom with options
-      $(":checked").each(function(){
-        console.log($(this).attr('id'))
-        })
+
       // Uncheked bottom
       $("#clear").click(function(){
-        $(":checked").each(function(){
+        $(".checkbox:checked").each(function(){
             $(this).prop("checked",false);
         });
       });
-      // // Send to Antique
-      // $("#clear").click(function(){
-      // Send to 
+      // Send to antique, reference in the table
+    $("#move_antique").click(function(){
+        var jobs = [];
+        $(".checkbox:checked").each(function(){
+            var thus = this;
+            jobs.push( Tablepolish.rows.filter(rows => rows.id == $(thus).attr("id"))[0] );
+            $(this).prop("checked",false);
+            
+        });
+        if(confirm("Are you sure?")){
+            for(let job in jobs)
+                Tablepolish.rows = Tablepolish.rows.filter(rows => rows.id != jobs[job].id);
+            socket.emit('sendto' + $(this).attr('id').split('_')[1], jobs);
 
+        }
+    // $(".modal-content").append('<p>' + JSON.stringify(job) + '</p>' );       
+    });
 
-
-    // var source = $("#tablepolish");
-    // var source = document.getElementById('tablepolish');
-    // var destination = document.getElementById('tableantique');
-    // var copy = source.cloneNode(true);
-    // copy.setAttribute('id', 'tableantique');
-    // destination.parentNode.replaceChild(copy, destination);
-
-    // var source = document.getElementById('tablepolish');
-    // var destination = document.getElementById('tablepainting');
-    // var copy = source.cloneNode(true);
-    // copy.setAttribute('id', 'tablepainting');
-    // destination.parentNode.replaceChild(copy, destination);
-
-
-// var mysql = require('../connexion/db_connexion');
-    
-//     var con = mysql.createConnection({
-//       host: "localhost:8080",
-//     //   user: "youruserQty",
-//     //   password: "yourpassword",
-//     //   database: "mydb"
-//     });
-    
-//     con.connect(function(err) {
-//       if (err) throw err;
-//       console.log("Connected!");
-//       var sql = "SELECT * FROM pegasus.products";
-//       con.query(sql, function (err, result) {
-//         if (err) throw err;
-//         console.log("Result");
-//       });
-//     });
-
-
-
+    // Send to Powder Coating, reference in the table
+    $("#move_powder").click(function(){
+      var jobs = [];
+      $(".checkbox:checked").each(function(){
+        var thus = this;
+        jobs.push(Tablepolish.rows.filter(rows => rows.id ==$(thus).attr("id"))[0]);
+        $(this).prop("checked",false);
+      });
+      if(confirm("Are you sure?")){
+        for(let job in jobs)
+          Tablepolish.rows = Tablepolish.rows.filter(rows => rows.id != jobs[job].id);
+        socket.emit('sendto'+ $(this).attr('id').split('_')[1],jobs);
+      }
+    });
+    // Finish process
+    $("#finish").click(function(){
+      var jobs = []
+      $(".checkbox:checked").each(function(){
+        var thus = this;
+        jobs.push(Tablepolish.rows.filter(rows => rows.id ==$(thus).attr("id"))[0]);
+        $(this).prop("checked",false);
+      });
+      if(confirm("Do you want to finish the process?")){
+        for(job in jobs)
+        Tablepolish.rows = Tablepolish.rows.filter(rows => rows.id != jobs[job].id);
+        socket.emit('sendto'+$(this).attr('id').split('_')[1],jobs);
+      }
+    });
+ 
 
 });
 
